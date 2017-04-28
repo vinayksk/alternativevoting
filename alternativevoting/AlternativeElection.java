2310cd4b6ff4c57132d55b43ba2b9dd450d68eee
lin-andrew <alin908@lhs.win.fuhsd.org>
2017-04-28 10:11:02 -0700

    Added a little code but it's flawed

--- alternativevoting/AlternativeElection.java
+++ alternativevoting/AlternativeElection.java
@@ -15,21 +15,42 @@ public class AlternativeElection
         ArrayList<Position> names = new ArrayList<Position>();
         System.out.println( "Enter the name of Position " + count + ":" );
         String str = in.nextLine();
-        names.add( new Position( str ) );
         while ( !str.equals( "!" ) )
         {
-
+            names.add( new Position( str ) );
             System.out.println( "Enter the name of Position " + ++count
                 + " or \"!\" if there are no more positions:" );
             str = in.nextLine();
-
         }
         for ( Position pos : names )
         {
-            System.out.println(
-                "Enter the name of Candidate " + ( pos.names.size() + 1 )
-                    + " for the position of " + pos.getName()
-                    + " or \"!\" to move to the next position or step" );
+            HashMap<Candidate, Integer> votes = new HashMap<Candidate, Integer>(); // THIS
+                                                                                   // IS
+                                                                                   // BAD
+                                                                                   // BECAUSE
+                                                                                   // WE
+                                                                                   // WANT
+                                                                                   // RANKINGS
+                                                                                   // NOT
+                                                                                   // JUST
+                                                                                   // INDIVUDUAL
+                                                                                   // VOTES
+            System.out.println( "Time for the election for the " + pos.getName()
+                + " position!" );
+            System.out.println( "Enter the name of Candidate 1:" );
+            String s = in.nextLine();
+            while ( !s.equals( "!" ) )
+            {
+                votes.put( new Candidate( s ), 0 );
+                System.out.println(
+                    "Enter the name of Candidate " + ( votes.size() + 1 )
+                        + " for the position of " + pos.getName()
+                        + " or \"!\" to move to the next position or step" );
+                s = in.nextLine();
+            }
+            System.out.println( "Time to input votes!" );
+            System.out
+                .println( "Please enter ranking of candidate in order of " );
         }
         System.out.println( "Goodbye!" );
         in.close();
