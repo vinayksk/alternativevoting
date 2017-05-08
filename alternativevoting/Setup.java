172988beba46e23e1dbbc605abab8bb9f18991be
lin-andrew <alin908@lhs.win.fuhsd.org>
2017-05-08 09:42:56 -0700

    ew

--- alternativevoting/Setup.java
+++ alternativevoting/Setup.java
@@ -9,10 +9,14 @@ public class Setup
 
     private ArrayList<Candidate> names;
 
+    private String electionName;
+
 
     public Setup()
     {
         Scanner in = new Scanner( System.in );
+        System.out.println( "What is the name of the election?" );
+        electionName = in.nextLine();
         int count = 1;
         names = new ArrayList<Candidate>();
         System.out.println( "Enter the name of Candidate " + count + ":" );
@@ -24,7 +28,6 @@ public class Setup
                 + " or \"!\" if there are no more candidates:" );
             str = in.nextLine();
         }
-        System.out.println( names );
     }
 
 
