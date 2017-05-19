c2c6ca0fea48c95c0ebf0ca6040c9064291961bc
lin-andrew <lin.andrew@gmail.com>
2017-05-19 10:43:03 -0700

    voter jsp

---
+++ alternativevoting/VoterApplet.java
@@ -0,0 +1,19 @@
+package alternativevoting;
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+
+/**
+ * Created by Andrew on 5/19/2017.
+ */
+public class VoterApplet extends JApplet
+{
+    public void init()
+    {
+        ArrayList<Candidate> list = new ArrayList<Candidate>();
+        list.add(new Candidate("Jeffrey", 3));
+        list.add(new Candidate("Vinay", 4));
+        ElectionScreen e = new ElectionScreen(list, "bad");
+        this.add(e);
+    }
+}
