143e2c4630f72105aa31b4ce9558792069630df1
jtao9 <jeffrey.tao9@gmail.com>
2017-05-28 23:06:47 -0700

    asdf

--- alternativevoting/AlternativeElectionTest.java
+++ alternativevoting/AlternativeElectionTest.java
@@ -7,25 +7,25 @@ public class AlternativeElectionTest
 {
     public static void main( String[] args )
     {
-        ArrayList<Candidate> list = new ArrayList<Candidate>();
-        Candidate alice = new Candidate( "Alice" );
-        Candidate bob = new Candidate( "Bob" );
-        Candidate charlie = new Candidate( "Charlie" );
-        list.add( alice );
-        list.add( bob );
-        list.add( charlie );
-        LinkedList<Queue<Candidate>> map = new LinkedList<Queue<Candidate>>();
-        Queue<Candidate> q = new LinkedList<Candidate>();
-        q.add( alice );
-        q.add( bob );
-        q.add( charlie );
-        map.add( q );
-        Queue<Candidate> q2 = new LinkedList<Candidate>();
-        q2.add( alice );
-        q2.add( charlie );
-        q2.add( bob );
-        map.add( q2 );
-        AlternativeElection a = new AlternativeElection( list, map );
-        System.out.println( a.eliminateCandidate() );
+//        ArrayList<Candidate> list = new ArrayList<Candidate>();
+//        Candidate alice = new Candidate( "Alice" );
+//        Candidate bob = new Candidate( "Bob" );
+//        Candidate charlie = new Candidate( "Charlie" );
+//        list.add( alice );
+//        list.add( bob );
+//        list.add( charlie );
+//        LinkedList<Queue<Candidate>> map = new LinkedList<Queue<Candidate>>();
+//        Queue<Candidate> q = new LinkedList<Candidate>();
+//        q.add( alice );
+//        q.add( bob );
+//        q.add( charlie );
+//        map.add( q );
+//        Queue<Candidate> q2 = new LinkedList<Candidate>();
+//        q2.add( alice );
+//        q2.add( charlie );
+//        q2.add( bob );
+//        map.add( q2 );
+//        AlternativeElection a = new AlternativeElection( list, map );
+//        System.out.println( a.eliminateCandidate() );
     }
 }
