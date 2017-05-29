f8c750d1380a5d8243e0587184d593dd981baf4b
vinayksk <vinayksk@gmail.com>
2017-05-29 10:01:20 -0700

    lit

--- alternativevoting/heroku.java
+++ alternativevoting/heroku.java
@@ -12,60 +12,11 @@ public class heroku {
     public heroku(){
 
     }
+
     public static void main(String[] args) throws InstantiationException,
             IllegalAccessException, ClassNotFoundException {
-        retrieve("future");
-//        ArrayList<String> cand = new ArrayList<>();
-//        cand.add("me");
-//        cand.add("you");
-//        createTable("aids", cand);
-//        System.out.println("-------- PostgreSQL "
-//                + "JDBC Connection Testing ------------");
-//        try {
-//            Class.forName("org.postgresql.Driver");
-//        } catch (ClassNotFoundException e) {
-//            System.out.println("Where is your PostgreSQL JDBC Driver? "
-//                    + "Include in your library path!");
-//            e.printStackTrace();
-//            return;
-//        }
-//        System.out.println("PostgreSQL JDBC Driver Registered!");
-//        Connection connection = null;
-//        try {
-//            Class.forName("org.postgresql.Driver");
-//            // ========>     from heroku website
-//            String url = String.format("jdbc:postgresql://ec2-54-163-254-76.compute-1.amazonaws.com:5432/dahhebb44gsieu?sslmode=require");
-//            Properties props = new Properties();
-//            props.setProperty("user", "ugtzulvykibvoo");
-//            props.setProperty("password", "288db9b457f62a3f7333f910087e6d79f38bd205a3e47841596d2658a93cfdbb");
-//            connection = DriverManager.getConnection(url, props);
-//            String lit = connection.getSchema();
-//            System.out.println("Success " + lit);
-//
-//            Statement stmt = connection.createStatement();
-////            String sql = "CREATE TABLE COMPANY " +
-////                    "(ID INT PRIMARY KEY     NOT NULL," +
-////                    " NAME           TEXT    NOT NULL, " +
-////                    " AGE            INT     NOT NULL, " +
-////                    " ADDRESS        CHAR(50), " +
-////                    " SALARY         REAL)";
-////            stmt.executeUpdate(sql);
-//            String fats = "insert into company (ID, name, age, address, salary) values (2, 'Jeff', 3, 22, 4)";
-//            stmt.executeUpdate(fats);
-//
-//            ResultSet myRs = stmt.executeQuery("select * from company");
-//
-//            while(myRs.next()){
-//                System.out.println(myRs.getString("name"));
-//            }
-//            stmt.close();
-//            connection.close();
-//        } catch (SQLException e) {
-//
-//            System.out.println("Connection Failed! Check output console");
-//            e.printStackTrace();
-//            return;
-//        }
+        System.out.print(retrieve("future"));
+
     }
 
     public void createTable(String electionName, ArrayList<String> candidates) throws InstantiationException,
@@ -129,7 +80,7 @@ public class heroku {
                 val.add(md.getColumnName(i));
             }
 
-            System.out.print("Values Retrieved");
+            System.out.println("Values Retrieved");
             stmt.close();
             connection.close();
             return val;
@@ -144,7 +95,7 @@ public class heroku {
 
     }
 
-    public static void add(Map<String, Integer> mp){
-
+    public static void request(String table){
+        
     }
 }
