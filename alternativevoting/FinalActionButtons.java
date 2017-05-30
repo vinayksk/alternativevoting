55aedd315d89bfe4364266db1c67935ea2f0d0ee
jtao9 <jeffrey.tao9@gmail.com>
2017-05-29 20:45:09 -0700

    asdfasdf

--- alternativevoting/FinalActionButtons.java
+++ alternativevoting/FinalActionButtons.java
@@ -1,18 +1,23 @@
 package alternativevoting;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
 public class FinalActionButtons extends JPanel implements ActionListener
 {
     private JButton eliminateButton, doneButton;
+
     ResultScreen r;
+
 
     public FinalActionButtons()
     {
@@ -21,32 +26,34 @@ public class FinalActionButtons extends JPanel implements ActionListener
         eliminateButton.addActionListener( this );
         add( eliminateButton );
 
-        doneButton = new JButton( "Done with Elimination" );
+        doneButton = new JButton( "Get Statistics" );
         doneButton.setPreferredSize( new Dimension( 200, 100 ) );
         doneButton.addActionListener( this );
         add( doneButton );
     }
 
-    public void setResultScreen(ResultScreen r)
+
+    public void setResultScreen( ResultScreen r )
     {
         this.r = r;
     }
+
 
     public void actionPerformed( ActionEvent e )
     {
         JButton button = (JButton)e.getSource();
         if ( button.getText().equals( "Eliminate a Candidate" ) )
         {
-            r.updateBarChart(r.getAlternativeElection().eliminateCandidate());
+            r.updateBarChart( r.getAlternativeElection().eliminateCandidate() );
         }
-        else if(button.getText().equals("Done with Elimination"))
+        else
         {
-            System.out.println("The winners are: \n");
-            ArrayList<Candidate> list = r.getAlternativeElection().getCandList();
-            for(int i=0;i<list.size();i++)
-            {
-                System.out.println(list.get(i).getName());
-            }
+            Component frame = new JFrame( "Election statistics" );
+            System.out.println();
+            JOptionPane.showMessageDialog( frame,
+                Arrays.deepToString( BallotStats.prefTable(
+                    r.getAlternativeElection().getBallots(),
+                    r.getAlternativeElection().getCandList() ) ) );
         }
     }
 }
--- alternativevoting/Statistics.java
+++ alternativevoting/Statistics.java
@@ -1,67 +1,67 @@
-package alternativevoting;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
-
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
-
-/**
- * Created by Andrew on 5/25/2017.
- */
-public class Statistics extends JPanel implements ActionListener
-{
-    private JButton statButton, explainButton;
-    ResultScreen r;
-
-    public Statistics()
-    {
-        statButton = new JButton( "Get Statistics" );
-        statButton.setPreferredSize( new Dimension( 200, 100 ) );
-        statButton.addActionListener( this );
-        add(statButton);
-
-        explainButton = new JButton( "Explanation" );
-        explainButton.setPreferredSize( new Dimension( 200, 100 ) );
-        explainButton.addActionListener( this );
-        add( explainButton );
-    }
-
-    public void setResultScreen(ResultScreen r)
-    {
-        this.r = r;
-    }
-
-    public void actionPerformed(ActionEvent e)
-    {
-        JButton button = (JButton)e.getSource();
-        if ( button.getText().equals( "Get Statistics" ) )
-        {
-            JFrame frame = new JFrame();
-            JLabel text = new JLabel("text");
-            JPanel panel = new JPanel();
-//            double[][] pref = BallotStats.pollProbabilities(r.getAlternativeElection().getBallots(), r.getAlternativeElection().getCandList());
-//            String str = "";
-//            for(int i=0;i<pref.length;i++)
-//            {
-//                for(int j=0;j<pref[0].length;j++)
-//                {
-//                    str += pref[i][j] + " ";
-//                }
-//                str += "\n";
-//            }
-           // text.setText(str);
-            text.setText("asdfk;lashfldahfklhdslfshflashlfdshlasdhfkladsh");
-            panel.add(text);
-            frame.add(panel, BorderLayout.CENTER);
-            frame.setBounds( 100, 100, 200, 200 );
-            frame.setDefaultCloseOperation( EXIT_ON_CLOSE );
-            frame.setVisible(true);
-        }
-        else if(button.getText().equals("Explanation"))
-        {
-            System.out.println("Ask Vinay");
-        }
-    }
-}
+//package alternativevoting;
+//
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import javax.swing.*;
+//
+//import static javax.swing.JFrame.EXIT_ON_CLOSE;
+//
+///**
+// * Created by Andrew on 5/25/2017.
+// */
+//public class Statistics extends JPanel implements ActionListener
+//{
+//    private JButton statButton, explainButton;
+//    ResultScreen r;
+//
+//    public Statistics()
+//    {
+//        statButton = new JButton( "Get Statistics" );
+//        statButton.setPreferredSize( new Dimension( 200, 100 ) );
+//        statButton.addActionListener( this );
+//        add(statButton);
+//
+//        explainButton = new JButton( "Explanation" );
+//        explainButton.setPreferredSize( new Dimension( 200, 100 ) );
+//        explainButton.addActionListener( this );
+//        add( explainButton );
+//    }
+//
+//    public void setResultScreen(ResultScreen r)
+//    {
+//        this.r = r;
+//    }
+//
+//    public void actionPerformed(ActionEvent e)
+//    {
+//        JButton button = (JButton)e.getSource();
+//        if ( button.getText().equals( "Get Statistics" ) )
+//        {
+//            JFrame frame = new JFrame();
+//            JLabel text = new JLabel("text");
+//            JPanel panel = new JPanel();
+////            double[][] pref = BallotStats.pollProbabilities(r.getAlternativeElection().getBallots(), r.getAlternativeElection().getCandList());
+////            String str = "";
+////            for(int i=0;i<pref.length;i++)
+////            {
+////                for(int j=0;j<pref[0].length;j++)
+////                {
+////                    str += pref[i][j] + " ";
+////                }
+////                str += "\n";
+////            }
+//           // text.setText(str);
+//            text.setText("asdfk;lashfldahfklhdslfshflashlfdshlasdhfkladsh");
+//            panel.add(text);
+//            frame.add(panel, BorderLayout.CENTER);
+//            frame.setBounds( 100, 100, 200, 200 );
+//            frame.setDefaultCloseOperation( EXIT_ON_CLOSE );
+//            frame.setVisible(true);
+//        }
+//        else if(button.getText().equals("Explanation"))
+//        {
+//            System.out.println("Ask Vinay");
+//        }
+//    }
+//}
